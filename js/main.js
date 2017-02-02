091efe5caa746b27c3e4a6ae79388d07c50e564b
Efren Vasquez <efrenvasquez@u.boisestate.edu>
2017-02-02 11:45:54 -0500

    fix errors in JS file

--- js/main.js
+++ js/main.js
@@ -1,47 +1,47 @@
 //Array
-var iceCream = ['Moose Tracks', 'Rocky Road', 'Cookies N Cream' ];
+var iceCream = ['Moose Tracks', 'Rocky Road', 'Cookies N Cream'];
 iceCream.push('Vanilla');
 
 //Object
 var georgeBush = {
-  firstName: 'George',
-  lastName: 'Bush',
-  termLength: '1',
-  party: 'Republican',
-  yearsOfPresidency: '4 years'
-}
+    firstName: 'George',
+    lastName: 'Bush',
+    termLength: '1',
+    party: 'Republican',
+    yearsOfPresidency: '4 years'
+};
 
 var billClinton = {
-  firstName: 'Bill',
-  lastName: 'Clinton',
-  termLength: '2',
-  party: 'Democrat',
-  yearsOfPresidency: '8 years'
-}
+    firstName: 'Bill',
+    lastName: 'Clinton',
+    termLength: '2',
+    party: 'Democrat',
+    yearsOfPresidency: '8 years'
+};
 
 var georgeWBush = {
-  firstName: 'George W.',
-  lastName: 'Bush',
-  termLength: '2',
-  party: 'Republican',
-  yearsOfPresidency: '8 years'
-}
+    firstName: 'George W.',
+    lastName: 'Bush',
+    termLength: '2',
+    party: 'Republican',
+    yearsOfPresidency: '8 years'
+};
 
 var barrackObama = {
-  firstName: 'Barrack',
-  lastName: 'Obama',
-  termLength: '2',
-  party: 'Democrat',
-  yearsOfPresidency: '8 years'
-}
+    firstName: 'Barrack',
+    lastName: 'Obama',
+    termLength: '2',
+    party: 'Democrat',
+    yearsOfPresidency: '8 years'
+};
 
 var donaldTrump = {
-  firstName: 'Donald',
-  lastName: 'Trump',
-  termLength: 'Current president',
-  party: 'Republican',
-  yearsOfPresidency: 'Currently in his first year'
-}
+    firstName: 'Donald',
+    lastName: 'Trump',
+    termLength: 'Current president',
+    party: 'Republican',
+    yearsOfPresidency: 'Currently in his first year'
+};
 
 var presidents = [georgeBush, billClinton, georgeWBush, barrackObama, donaldTrump];
 console.log(presidents[2].firstName + presidents[2].lastName);
