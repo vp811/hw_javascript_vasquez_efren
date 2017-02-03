ac348a4d4e12f1f57411e186b2730965521b960e
Efren Vasquez <efrenvasquez@u.boisestate.edu>
2017-02-03 14:47:14 -0500

    add functions branch and solution

--- js/main.js
+++ js/main.js
@@ -45,3 +45,37 @@ var donaldTrump = {
 
 var presidents = [georgeBush, billClinton, georgeWBush, barrackObama, donaldTrump];
 console.log(presidents[2].firstName + presidents[2].lastName);
+
+//Fuctions
+//Function Declaration
+function howdy(name, greeting) {
+    return name + ' says ' + greeting;
+}
+var message = howdy('Efren Vasquez', 'hi!');
+console.log(message);
+
+//Fuction Expression
+var numba = function (string) {
+    if (string.length < 7) {
+        return 'What a short little word!';
+    } else if (string.length > 7) {
+        return "I'm sorry, but that's too many to count.";
+    } else {
+        return '7, what a perfect choice!';
+    }
+};
+
+console.log(numba('7'));
+console.log(numba('seventy'));
+console.log(numba('university'));
+
+//Inception function
+function inception(display, favMovie) {
+    display(favMovie);
+}
+
+var display = function (favMovie) {
+    console.log(favMovie + ' is a fantastic movie!');
+};
+
+inception(display, 'Star Wars');
